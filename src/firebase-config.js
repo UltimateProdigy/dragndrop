32a5c29ca9bcafc457bc896a75ec16462d77333c
UltimateProdigy <ayobamiakinola84@gmail.com>
2023-09-20 22:51:49 +0100

    Changes

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDLqNdlJdpD8y9wBbF0Xo23WMEQlhJ7zv4",
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: "dragndrop-c88c6.firebaseapp.com",
     projectId: "dragndrop-c88c6",
     storageBucket: "dragndrop-c88c6.appspot.com",
